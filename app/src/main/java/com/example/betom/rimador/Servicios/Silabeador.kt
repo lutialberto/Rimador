9d29546f5ff0569203420f3598b1c34670f74ef9
Alberto Luti <betomdq.93@gmail.com>
2018-09-18 10:43:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/betom/rimador/Servicios/Silabeador.kt
@@ -0,0 +1,12 @@
+package com.example.betom.rimador.Servicios
+
+object Silabeador {
+
+    var silabas = ArrayList<String>()
+    lateinit var palabra : String
+
+    fun separar (cadena:String) {
+        var maquinaEstados = Array(14,{arrayof})
+    }
+
+}
