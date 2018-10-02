4dc1a4b512a5d1c532a9fa172d0aee0ad24d0251
Alberto Luti <betomdq.93@gmail.com>
2018-10-02 17:10:20 -0300

    new rhyme buttons with constrains and styles added

--- app/src/main/java/com/example/betom/rimador/utilities/Constants.kt
+++ app/src/main/java/com/example/betom/rimador/utilities/Constants.kt
@@ -1,18 +1,18 @@
 package com.example.betom.rimador.utilities
 
-/*informacion sobre la maquina de estados para separarEnSilabas en syllables*/
+/*informacion sobre la maquina de estados para separar en silabas */
 const val LETTER_SETS_COUNT = 11
 const val END_OF_WORD_DELIMITER = " "
 
 /*informacion sobre constantes de la clase palabra*/
 const val NO_ERRORS = ""
 
-/*mensajes separando en syllables*/
+/*mensajes separando en silabas*/
 const val ERROR_WORD_WITHOUT_VOCALS = "No existen palabras sin vocales"
-const val ERROR_UNKNOWN_SYMBOL = "Word con simbolos no permitidos -> "
+const val ERROR_UNKNOWN_SYMBOL = "Palabra con simbolos no permitidos -> "
 const val ERROR_EMPTY_WORD = "Palabra vacia"
 
-/*mensajes syllables tonica o con tilde*/
+/*mensajes con silaba tonica o con tilde*/
 const val ERROR_TO_MANY_ACCENT_MARKS = "No puede haber mas de una vocal con tilde por palabra"
 const val WORD_WITHOUT_ACCENT_MARK = -1
 const val TONIC_SYLLABLE_DEFAULT_VALUE = -2
