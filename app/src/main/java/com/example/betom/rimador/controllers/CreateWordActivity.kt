f1c786bab89fe2ad900bb8360a274d663546614c
Alberto Luti <betomdq.93@gmail.com>
2018-10-27 21:55:59 -0300

    create word activity created

---
+++ app/src/main/java/com/example/betom/rimador/controllers/CreateWordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.betom.rimador.controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.betom.rimador.R
+
+class CreateWordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_word)
+    }
+}
